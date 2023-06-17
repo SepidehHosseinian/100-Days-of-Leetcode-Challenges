ecfd55e1c5ffe998659b844250e2e8fa78fb4a8d
Sepideh Hosseinian <s.sepideh.hoseinian@gmail.com>
2023-06-17 08:56:59 +0330

    Create 2154_keep_multiplying_found_values_by_two.py

---
+++ DS/Array101/2154_keep_multiplying_found_values_by_two.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        while original in nums:
+            original = original*2
+        else:
+            return original
+    #2154
