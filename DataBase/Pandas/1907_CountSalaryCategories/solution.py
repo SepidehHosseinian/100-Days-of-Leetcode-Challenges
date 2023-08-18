9c9a4ffffddcfd082958ee60b624e07c067e7849
sepideh <s.sepideh.hoseinian@gmail.com>
2023-08-18 22:58:29 +0330

    1907. Count Salary Categories

---
+++ DataBase/Pandas/1907_CountSalaryCategories/solution.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def count_salary_categories(accounts: pd.DataFrame) -> pd.DataFrame:
+    return pd.DataFrame({
+        'category': ['Low Salary', 'Average Salary', 'High Salary'],
+        'accounts_count': [
+            accounts[accounts.income < 20000].shape[0],
+            accounts[(accounts.income >= 20000) & (accounts.income <= 50000)].shape[0],
+            accounts[accounts.income > 50000].shape[0],
+        ],
+    })
