94bf54cd121f93b43da99d6dd4f3a6ab5f626f49
spidehhosseinian <s.sepideh.hoseinian@gmail.com>
2023-12-24 14:22:44 +0330

    144_BinaryTreePreorderTraversal

---
+++ DataStractures/Binary Tree/144_BinaryTreePreorderTraversal/solution.py
@@ -0,0 +1,14 @@
+#Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def preorderTraversal(self, root: [TreeNode]) -> list[int]:
+        result = []
+        if root != None:
+            result.append(root.val)
+            result.extend(self.preorderTraversal(root.left))
+            result.extend(self.preorderTraversal(root.right))
+        return result
